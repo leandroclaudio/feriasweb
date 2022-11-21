ddbe26561cfe89da5788afac1b36c86e3b1cb014
leandroclaudio <leandroclaudio@gmail.com>
2022-11-21 18:08:59 -0300

    segunda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MarcaFerias from './components/MarcaFerias/MarcaFerias';
+import Calendar  from './components/Calendario/Calendar';
+import Colaboradores from './components/Colaboradores/Colaboradores';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MarcaFerias />
+        {/* <Calendar /> */}
+        {/* <Colaboradores /> */}
     </div>
   );
 }
